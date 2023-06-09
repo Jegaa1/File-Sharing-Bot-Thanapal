e7ce02b12d872f5644b864d171a2f29aa9287cee
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-09 15:55:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "880087645"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://eric:eric@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "myserialbot")
 
 #force sub channel id, if you want enable force sub
