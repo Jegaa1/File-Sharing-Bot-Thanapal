395b64f4da6edddce344d2bf58b6a7126e267ff0
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-21 20:38:46 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -25,10 +25,16 @@ async def channel_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://telegram.me/{client.username}?start={base64_string}"
+    # get media type
+    media = message.document or message.video or message.audio or message.photo
+    # get file name
+    file_name = media.file_name if media.file_name else ""
+    # get caption (if any)
+    caption = message.caption if media.file_name else ""
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(f"<b>Link</b>: {link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>{file_name}:\n\nLink: {link}</b>", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
