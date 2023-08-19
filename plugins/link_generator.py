46ab5f5b25cc531c7715d8ea79f4b64ca852c9ea
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-08-19 20:19:21 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -56,4 +56,4 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://telegram.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>{caption} - {file_size}MB\n\nLink: {link}</b>", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>{caption} - {file_size}\n\nLink: {link}</b>", quote=True, reply_markup=reply_markup)
