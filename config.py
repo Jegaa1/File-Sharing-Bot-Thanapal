a0e925df1f3a2adaebe9e6d907a09cd13192273a
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-10-10 07:18:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.
 DB_NAME = os.environ.get("DATABASE_NAME", "leecher")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001940001862"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001985606821"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
