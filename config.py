50074cf83634b09dceacdf5b8fcb0b07f1c71b7c
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-12-03 07:47:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.
 DB_NAME = os.environ.get("DATABASE_NAME", "leecher")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001985606821"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002028699133"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "8"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
